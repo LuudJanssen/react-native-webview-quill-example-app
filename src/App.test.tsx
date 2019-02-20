6fd96ddbf724edbcfbb56fd7b1937b62e5786fa1
luudjanssen <luud-@live.nl>
2019-02-20 15:19:13 +0100

    Run Prettier

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,10 @@
+import * as React from 'react';
 import 'react-native';
-import React from 'react';
 import App from './App';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import reactTestRenderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  reactTestRenderer.create(<App />);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * 
+ *
  * Generated with the TypeScript template
  * https://github.com/emin93/react-native-template-typescript
- * 
+ *
  * @format
  */
 
 import * as React from 'react';
-import { Hello } from 'react-native-webview-quill'
+import { Hello } from 'react-native-webview-quill';
 
 export default class App extends React.Component {
-  render() {
-    return (<Hello></Hello>);
+  public render() {
+    return <Hello />;
   }
 }
